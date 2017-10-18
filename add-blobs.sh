a21f664702808e86de914ac76d919eb87f0f5e39
making <makingx@gmail.com>
2017-10-18 21:14:22 +0900

    Add add-blobs.sh

---
+++ add-blobs.sh
@@ -0,0 +1,21 @@
+#!/bin/sh
+
+DIR=`pwd`
+
+mkdir -p .downloads
+
+cd .downloads
+
+
+
+if [ ! -f ${DIR}/blobs/java/openjdk-1.8.0_141.tar.gz ];then
+    curl -L -O -J https://download.run.pivotal.io/openjdk-jdk/trusty/x86_64/openjdk-1.8.0_141.tar.gz
+    bosh add-blob --dir=${DIR} openjdk-1.8.0_141.tar.gz java/openjdk-1.8.0_141.tar.gz
+fi
+
+if [ ! -f ${DIR}/blobs/nexus/nexus-3.5.0-02-unix.tar.gz ];then
+    curl -L -O -J https://sonatype-download.global.ssl.fastly.net/nexus/3/nexus-3.5.0-02-unix.tar.gz
+    bosh add-blob --dir=${DIR} nexus-3.5.0-02-unix.tar.gz nexus/nexus-3.5.0-02-unix.tar.gz
+fi
+
+cd -
