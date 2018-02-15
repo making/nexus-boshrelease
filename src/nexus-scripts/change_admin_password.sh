c9c6aaf640e46d28eacfe80938dac1543f3a1302
Toshiaki Maki <tmaki@pivotal.io>
2018-02-15 15:01:04 +0900

    Fix wrong commit

--- src/nexus-scripts/change_admin_password.sh
+++ src/nexus-scripts/change_admin_password.sh
@@ -16,23 +16,23 @@ function change_admin_password() {
 }
 EOF
 
-  CHECK_SCRIPT_STATUS=`curl ${CURL_OPTS} -s -o /dev/null -I -w "%{http_code}" -u "admin:${NEXUS_OLD_PWD}" "${NEXUS_URL}/service/siesta/rest/v1/script/${SCRIPT_NAME}"`
+  CHECK_SCRIPT_STATUS=`curl ${CURL_OPTS} -s -o /dev/null -I -w "%{http_code}" -u "admin:${NEXUS_OLD_PWD}" "${NEXUS_URL}/service/rest/v1/script/${SCRIPT_NAME}"`
 
   if [ "${CHECK_SCRIPT_STATUS}" == "404" ];then
     echo "> ${SCRIPT_NAME} is not found (${CHECK_SCRIPT_STATUS})"
     echo "> creating script (${SCRIPT_NAME}) ..."
-    curl ${CURL_OPTS} -H "Accept: application/json" -H "Content-Type: application/json" -d "${SCRIPT_JSON}" -u "admin:${NEXUS_OLD_PWD}" "${NEXUS_URL}/service/siesta/rest/v1/script/"
+    curl ${CURL_OPTS} -H "Accept: application/json" -H "Content-Type: application/json" -d "${SCRIPT_JSON}" -u "admin:${NEXUS_OLD_PWD}" "${NEXUS_URL}/service/rest/v1/script/"
   elif [ "${CHECK_SCRIPT_STATUS}" == "401" ];then
     echo "> Unauthorized (${CHECK_SCRIPT_STATUS})"
     return
   else
     echo "> ${SCRIPT_NAME} is found (${CHECK_SCRIPT_STATUS})"
     echo "> updating script (${SCRIPT_NAME}) ..."
-    curl ${CURL_OPTS} -XPUT -H "Accept: application/json" -H "Content-Type: application/json" -d "${SCRIPT_JSON}" -u "admin:${NEXUS_OLD_PWD}" "${NEXUS_URL}/service/siesta/rest/v1/script/${SCRIPT_NAME}"
+    curl ${CURL_OPTS} -XPUT -H "Accept: application/json" -H "Content-Type: application/json" -d "${SCRIPT_JSON}" -u "admin:${NEXUS_OLD_PWD}" "${NEXUS_URL}/service/rest/v1/script/${SCRIPT_NAME}"
   fi
 
   echo "> updating password ..."
-  CHECK_RUN_STATUS=`curl ${CURL_OPTS} -s -o /dev/null -w "%{http_code}" -H "Content-Type: text/plain" -u "admin:${NEXUS_OLD_PWD}" "${NEXUS_URL}/service/siesta/rest/v1/script/${SCRIPT_NAME}/run" -d "${NEXUS_NEW_PWD}"`
+  CHECK_RUN_STATUS=`curl ${CURL_OPTS} -s -o /dev/null -w "%{http_code}" -H "Content-Type: text/plain" -u "admin:${NEXUS_OLD_PWD}" "${NEXUS_URL}/service/rest/v1/script/${SCRIPT_NAME}/run" -d "${NEXUS_NEW_PWD}"`
 
   if [ "${CHECK_RUN_STATUS}" == "200" ];then
     echo "> succeeded!"
