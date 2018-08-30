c80a42304c36367687fdcbfcb1d5b553ccac42ea
Toshiaki Maki <tmaki@pivotal.io>
2018-08-30 19:54:56 +0900

    No more dowloading JDK

--- add-blobs.sh
+++ add-blobs.sh
@@ -8,11 +8,6 @@ cd .downloads
 
 
 
-if [ ! -f ${DIR}/blobs/java/openjdk-1.8.0_172.tar.gz ];then
-    curl -L -O -J https://download.run.pivotal.io/openjdk-jdk/trusty/x86_64/openjdk-1.8.0_172.tar.gz
-    bosh add-blob --dir=${DIR} openjdk-1.8.0_172.tar.gz java/openjdk-1.8.0_172.tar.gz
-fi
-
 if [ ! -f ${DIR}/blobs/nexus/nexus-3.12.1-01-unix.tar.gz ];then
     curl -L -O -J https://sonatype-download.global.ssl.fastly.net/nexus/3/nexus-3.12.1-01-unix.tar.gz
     bosh add-blob --dir=${DIR} nexus-3.12.1-01-unix.tar.gz nexus/nexus-3.12.1-01-unix.tar.gz
